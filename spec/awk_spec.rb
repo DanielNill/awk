d51444cea0e22e9d6fe27627a6fc3d9ff61130c1
DanielN <daniellnill@gmail.com>
2017-01-25 23:18:17 -0800

    add spec basics

---
+++ spec/awk_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe Awk::Awk do
+  context 'feature' do
+    it 'evaluates a file' do
+    end
+
+    it 'evalutates a string' do
+    end
+
+    it 'pairs a patterns and and action' do
+    end
+
+    it 'perform executes all patterns and actions for the data given' do
+    end
+
+    it 'provides the begin keyword' do
+    end
+
+    it 'provides the end keyword' do
+    end
+
+    it 'provides the nr keyword' do
+    end
+
+    it 'provides the nf keyword' do
+    end
+
+    it 'assumes a pattern of true for an action without a matched pattern' do
+    end
+
+    it 'ignores a pattern given without a matched action' do
+    end
+  end
+
+  context 'unit' do
+  end
+end
